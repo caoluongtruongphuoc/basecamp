418de8d6737b15117e939d57822dfc8d4ed38f49
chithanh1998 <chithanh1998@gmail.com>
2021-05-24 15:15:37 +0700

    up

--- app/design/frontend/AHT/basecamp/web/js/main.js
+++ app/design/frontend/AHT/basecamp/web/js/main.js
@@ -3,7 +3,7 @@ require([
     'slick'
 ], function ($) {
     $(document).ready(function () {
-        $(".categories").slick({
+        $(".categories").not('.slick-initialized').slick({
             //autoplay: true,
             slidesToShow: 5,
             slidesToScroll: 1,
@@ -36,6 +36,59 @@ require([
                     breakpoint: 640,
                     settings: {
                         slidesToShow: 2,
+                        slidesToScroll: 2,
+                        dots: false,
+                        arrows: false,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 380,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        dots: false,
+                        arrows: false,
+                        infinite: true
+                    }
+                }
+            ]
+        });
+
+        $(".brand").not('.slick-initialized').slick({
+            //autoplay: true,
+            slidesToShow: 8,
+            slidesToScroll: 1,
+            dots: false,
+            infinite: true,
+            arrows: false,
+            speed: 300,
+            //variableWidth: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 1,
+                        dots: false,
+                        arrows: false,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        dots: false,
+                        arrows: false,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 3,
                         slidesToScroll: 2,
                         dots: false,
                         arrows: false,
